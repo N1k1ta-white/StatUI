9045dda4d0fefb31da885bb20325798a793f2e3c
Никита <65460812+N1k1ta-white@users.noreply.github.com>
2025-03-15 12:27:59 +0200

    Implement file upload functionality with CSV validation and update server port to 5000

--- backend/organizer/src/app.controller.ts
+++ backend/organizer/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+import { Express } from 'express';
+import { FileUploadInterceptor } from './interceptors/file.interceptor';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    const url = 'http://127.0.0.1:5001'
+    const response = await lastValueFrom(this.httpService.get(url));
+    console.log(response.data);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileUploadInterceptor.getInterceptor())
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 }
