acb85f6c50d485a1b16b86a1611ddb4a5a29660f
Kristian2109 <kristian.petrov1998@gmail.com>
2025-03-15 17:41:00 +0200

    Add cors everywhere

--- backend/statistics/app.py
+++ backend/statistics/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from dotenv import load_dotenv
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from werkzeug.datastructures import FileStorage
 
 from descriptive.descriptive import getDescriptiveStatistics, getGraphics, clustering
@@ -12,17 +12,20 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/descriptive", methods=["POST"])
+@cross_origin()
 def descriptiveStatistics():
     file: FileStorage = request.files["file"]
     statistics = getDescriptiveStatistics(file)
     return statistics
 
 @app.route("/graphics", methods=["POST"])
+@cross_origin()
 def graphics():
     file: FileStorage = request.files["file"]
     graphics = getGraphics(file)
     return graphics
 
+@cross_origin()
 @app.route("/clustering", methods=["POST"])
 def createClusters():
     file: FileStorage = request.files["file"]
