2c7da0ac961930b58b8c79d4dbc12432031aa3f5
Kristian2109 <kristian.petrov1998@gmail.com>
2025-03-15 11:30:54 +0200

    Add environemnt

--- backend/statistics/app.py
+++ backend/statistics/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return {
+        "max": 100
+    }
+
+if __name__ == "__main__":
+    app.run()
