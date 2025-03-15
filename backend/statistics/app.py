9f5bfb107b2df9954291c20a31dca085a2ba7fbc
Kristian2109 <kristian.petrov1998@gmail.com>
2025-03-15 16:12:26 +0200

    Refactor the env and add graphcis

--- backend/statistics/app.py
+++ backend/statistics/app.py
@@ -2,17 +2,23 @@ from flask import Flask, request
 from dotenv import load_dotenv
 from werkzeug.datastructures import FileStorage
 
-from descriptive.descriptive import getDescriptiveStatistics
+from descriptive.descriptive import getDescriptiveStatistics, getGraphics
 
 load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/descriptive", methods=["POST"])
-def hello_world():
+def descriptiveStatistics():
     file: FileStorage = request.files["file"]
     statistics = getDescriptiveStatistics(file)
     return statistics
+
+@app.route("/graphics", methods = ["POST"])
+def graphics():
+    file: FileStorage = request.files["file"]
+    graphics = getGraphics(file)
+    return graphics
 
 if __name__ == "__main__":
     app.run()
--- backend/statistics/descriptive/descriptive.py
+++ backend/statistics/descriptive/descriptive.py
@@ -2,12 +2,16 @@ import pandas as pd
 import json
 from .openAiClient import OPEN_AI_CLIENT
 from .selectMetricsPrompt import selectMetricsPrompt
+from .generateGraphicsPrompt import generateGraphicsPrompt
 from werkzeug.datastructures import FileStorage
 
 def getDescriptiveStatistics(file: FileStorage):
     df = pd.read_csv(file)
     prompt = selectMetricsPrompt(df)
-    response = OPEN_AI_CLIENT.generate_response(prompt)
-    print(response)
-    json_string = response.replace("```json\n", "").replace("```", "").strip()
-    return json.loads(json_string)
+    return OPEN_AI_CLIENT.generate_response(prompt)
+
+def getGraphics(file: FileStorage):
+    df = pd.read_csv(file)
+    prompt = generateGraphicsPrompt(df)
+    return OPEN_AI_CLIENT.generate_response(prompt)
+
