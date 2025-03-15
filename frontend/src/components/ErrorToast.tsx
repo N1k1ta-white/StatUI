bf90948cb246fba833db2a798eb22189a75f1390
Nikolai <nikoliktodorov@gmail.com>
2025-03-16 00:28:36 +0200

    toast added

--- frontend/src/components/ErrorToast.tsx
+++ frontend/src/components/ErrorToast.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store.ts";
 
-interface Props {}
+import { Toaster } from "@/components/ui/sonner";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
-function ErrorToast(props: Props) {
- return (
-  <div></div>
- );}
+const ErrorToast: React.FC = () => {
+
+    const error = useSelector((state: RootState) =>
+        Object.values(state).find(slice => slice.error)?.error
+    );
+
+    useEffect(() => {
+        if (error) {
+            toast("Error occurred", {
+                description: error,
+                action: {
+                    label: "GOT IT",
+                    onClick: () => console.log(error.message),
+                },
+                icon: "",
+                style: {
+                    backgroundColor: "#92010a",
+                    color: "#ffffff",
+                    border: "1px solid #f5c6cb",
+                },
+            });
+           ;        }
+    }, [error]);
+
+    return <Toaster />;
+};
 
 export default ErrorToast;
