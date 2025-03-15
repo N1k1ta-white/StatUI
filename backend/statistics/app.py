cc6dd07145ea99b955975485502cf9817cf592c2
Kristian2109 <kristian.petrov1998@gmail.com>
2025-03-15 16:48:32 +0200

    Add clustering

--- backend/statistics/app.py
+++ backend/statistics/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from dotenv import load_dotenv
 from werkzeug.datastructures import FileStorage
 
-from descriptive.descriptive import getDescriptiveStatistics, getGraphics
+from descriptive.descriptive import getDescriptiveStatistics, getGraphics, clustering
 
 load_dotenv()
 
@@ -14,11 +14,19 @@ def descriptiveStatistics():
     statistics = getDescriptiveStatistics(file)
     return statistics
 
-@app.route("/graphics", methods = ["POST"])
+@app.route("/graphics", methods=["POST"])
 def graphics():
     file: FileStorage = request.files["file"]
     graphics = getGraphics(file)
     return graphics
+
+@app.route("/clustering", methods=["POST"])
+def createClusters():
+    file: FileStorage = request.files["file"]
+    clusters = clustering(file)
+    return {
+        "clusters": clusters
+    }
 
 if __name__ == "__main__":
     app.run()
